4308e22cc2bbefabd539de6555e7ff59b1063d74
szj444 <2470937780@qq.com>
2025-04-27 15:29:31 +0800

    Add files via upload

---
+++ one_week/source code/demo/src/Graduate.java
@@ -0,0 +1,7 @@
+package com.extend_.improve_;
+
+public class Graduate extends student{
+    public void test(){
+        System.out.println("大学生"+name+"的考试成绩");
+    }
+}
---
+++ one_week/source code/demo/src/Pupil.java
@@ -0,0 +1,9 @@
+package com.extend_.improve_;
+
+
+public class Pupil extends student{
+    public void test(){
+        System.out.println("小学生"+name+"的考试成绩");
+    }
+}
+
