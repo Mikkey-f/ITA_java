7955320fbb80510ba1a822bfb0436260e437ae9c
lixinran1006 <q18782971845@126.com>
2025-05-08 21:53:34 +0800

    Add files via upload

---
+++ week-2/day2leibulei/Animal.java
@@ -0,0 +1,5 @@
+package day2leibulei;
+
+public abstract class Animal {
+    public abstract void eat();
+}
---
+++ week-2/day2leibulei/human.java
@@ -0,0 +1,8 @@
+package day2leibulei;
+
+public class human extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("米饭");
+    }
+}
